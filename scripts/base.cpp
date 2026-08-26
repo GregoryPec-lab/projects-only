d20670ed2288f07de57550e5a00c8f1e3c9d9261
Gregory <gpecklun@gmail.com>
2026-08-26 22:16:06 +0300

    Make better base.cpp

--- scripts/base.cpp
+++ scripts/base.cpp
@@ -2,53 +2,76 @@
 #include <string>
 using namespace std;
 
-struct people {
+struct People {
     float age;
     int id;
     string name;
 
-    void insert(const float &ageIns, const float &idIns, const string &nameIns) {
+    void insert(float ageIns, int idIns, const string& nameIns) {
         age = ageIns;
         name = nameIns;
         id = idIns;
     }
 
-    void searchId(int &searchId){
-        if(searchId == id) { cout << "Name: " << name << '\n' << "Age: " << age << '\n';}
+    void searchId(int searchId) {
+        if (searchId == id) {
+            cout << "Name: " << name << '\n';
+            cout << "Age: " << age << '\n';
+        }
     }
-    void searchName(int &searchName){
-        if(searchName == name) { cout << "ID: " << id << '\n' << "Age: " << age << '\n';}
-    }
-    void searchAge(int &searchAge){
-        if(searchAge == Age) { cout << "Name: " << name << '\n' << "ID: " << id << '\n';}
-    }
-}
 
-bool isAdmin(){
-    string bool;
+    void searchName(const string& searchName) {
+        if (searchName == name) {
+            cout << "ID: " << id << '\n';
+            cout << "Age: " << age << '\n';
+        }
+    }
+
+    void searchAge(float searchAge) {
+        if (searchAge == age) {
+            cout << "Name: " << name << '\n';
+            cout << "ID: " << id << '\n';
+        }
+    }
+};
+
+bool isAdmin() {
+    string role;
     string pass;
-    cout<<"Are you (G)uest or (A)dmin? Answer is ONLY G or A :";
-    cin>>bool;
-    if(bool == "A"){
-        cout<<"Password:"
-        cin>>pass;
-        if(pass == "123"){cout<<"Acces granted" << '\n';
+
+    cout << "Are you (G)uest or (A)dmin? Answer is ONLY G or A: ";
+    cin >> role;
+
+    if (role == "A") {
+        cout << "Password: ";
+        cin >> pass;
+
+        if (pass == "123") {
+            cout << "Access granted\n";
             return true;
         }
-    }
-    if(bool == "G"){
+
+        cout << "Wrong password!\n";
         return false;
     }
+
+    if (role == "G") {
+        return false;
+    }
+
+    cout << "Invalid role!\n";
+    return false;
 }
 
-int main(){
-    cout<< "C++ base" << '\n';
-    while(true){
-        if(isAdmin()){
+int main() {
+    cout << "C++ base\n";
 
+    while (true) {
+        if (isAdmin()) {
+            cout << "You're ADMIN!\n";
         }
-        else{
-
+        else {
+            cout << "You're GUEST!\n";
         }
     }
 }
